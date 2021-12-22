6a7f4aa57af969b6d1296ac48176458ffb409a0b
Shaik Muneer <shaik.muneer@aveva.com>
2021-12-22 11:41:40 +0530

    First checkin

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class even
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine(i + "*" + j + "=" + i * j);
+                }
+                Console.WriteLine("*****");
+            }
+
+        }
+    }
+}
+
